48237ac150c5be0b9b2522afad33d9d93dff273a
Valery Basova <basovavalery@gmail.com>
2022-05-18 12:17:07 -0400

    add dependencies and LoginFragment

---
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.udacity.shoestore.databinding.ActivityMainBinding
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
---
+++ app/src/main/java/com/udacity/shoestore/fragments/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.udacity.shoestore.fragments
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
