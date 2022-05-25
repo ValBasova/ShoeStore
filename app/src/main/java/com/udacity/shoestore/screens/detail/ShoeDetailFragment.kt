eee6da2b811ad1fcae93279e318480ccefd4bf49
Valery Basova <basovavalery@gmail.com>
2022-05-24 23:53:19 -0400

    add save and cancel button listreners

--- app/src/main/java/com/udacity/shoestore/screens/detail/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/detail/ShoeDetailFragment.kt
@@ -6,12 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.ShoeViewModel
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
+import com.udacity.shoestore.models.Shoe
 
 
 class ShoeDetailFragment : Fragment() {
     private lateinit var binding: FragmentShoeDetailBinding
+    private val viewModel: ShoeViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +28,20 @@ class ShoeDetailFragment : Fragment() {
             container,
             false
         )
+        binding.saveButton.setOnClickListener {
+            val newShoe = Shoe(
+                binding.shoeNameText.text.toString(),
+                binding.shoeSizeText.text.toString().toDouble(),
+                binding.shoeCompanyText.text.toString(),
+                binding.shoeDescriptionText.text.toString()
+            )
+            viewModel.addNewShoe(newShoe)
+            findNavController().navigate(R.id.action_shoeDetailFragment_to_inventoryFragment)
+
+        }
+        binding.cancelButton.setOnClickListener {
+            findNavController().navigate(R.id.action_shoeDetailFragment_to_inventoryFragment)
+        }
         return binding.root
     }
 }
